05fb1e37413adc4dffd6e2bd45c8bcd2fdabbc3a
POLLY1515 <amarantepolly2030@gmail.com>
2023-03-13 00:16:20 -0300

    Ex:5

--- src/EstruturaSequencial/Ex5.java
+++ src/EstruturaSequencial/Ex5.java
@@ -5,19 +5,31 @@ import java.util.Scanner;
 public class Ex5 {
 
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
+
+		double precoUnitario , valorPago, valorTotal, troco;
+		int quantidade;
 		
-		System.out.println("Entre com a base do retangulo:");
-		double base = sc.nextDouble();
-		System.out.println("Entre com a altura do retangulo:");
-		double altura = sc.nextDouble();
 		
-		double area = base * altura;
-		double perimetro = 2 *(base + altura);
+		System.out.println("Entre com o preço da unidade:");
+		precoUnitario = sc.nextDouble();
+		System.out.println("Entre com a quantidade comprada:");
+		quantidade = sc.nextInt();
 		
-		System.out.printf("Area: %.4f\n ",area);
-		System.out.printf("Perímertro: %.4f\n", perimetro);
+		 valorTotal = precoUnitario * quantidade;
+
+		System.out.printf("Valor a pagar R$:%.2f\n", valorTotal);
+		System.out.println("Entre com o pagamento:");
+		valorPago = sc.nextDouble();
+		 troco =  valorPago - valorTotal ;
+		 
+		 System.out.printf("Preço unitário do produto R$: %.2f reais \n" , precoUnitario);
+		 System.out.println("Quantidade comprada:" + quantidade + " unidade(s)");
+		 System.out.printf("Dinheiro recebido R$:%.2f reais \n" , valorPago );
+		 System.out.printf("Troco R$: %.2f reais \n", troco );
+
+
 		
 	}
 
